e569a01759c8d9e3bc7430e7e48d94eb32087a1e
ChoJinmok <jjmok1127@gmail.com>
2022-11-25 03:23:13 +0900

    Add: sign up page E2E 테스트에서 input 항목 추가

--- test/signup_test.js
+++ test/signup_test.js
@@ -1,7 +1,22 @@
 Feature('signup');
 
-Scenario('test something', ({ I }) => {
+const signUpInputNames = [
+  'name',
+  'phoneNumber',
+  'gender',
+  'streetNameAddress',
+  'detailedAddress',
+  'zipCode',
+  'email',
+  'password',
+];
+
+Scenario('회원 가입 페이지 제목과 각 회원 가입 필요 항목들을 볼 수 있다.', ({ I }) => {
   I.amOnPage('/signup');
 
   I.see('IKEHAEYEO Family 회원 가입');
+
+  signUpInputNames.forEach((inputName) => {
+    I.see(inputName);
+  });
 });
