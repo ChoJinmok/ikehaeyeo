59f2634fc7326c8ee3546987fcbd8f673befe9cb
ChoJinmok <jjmok1127@gmail.com>
2022-12-15 01:46:12 +0900

    Add: Font Awesome 설치 및 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,14 @@ import { Global } from '@emotion/react';
 
 import type { AppProps } from 'next/app';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 import globals from '../styles/globals';
 
 import wrapper from '../store';
+
+config.autoAddCss = false;
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
